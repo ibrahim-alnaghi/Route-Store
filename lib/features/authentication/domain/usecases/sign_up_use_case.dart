e20f31586b7eeded28ef8a25a2c1c1dfd4bdb9d4
Ibrahim Alnaghi <alnaghy08@gmail.com>
2025-06-29 09:30:18 +0300

    fix import path

--- lib/features/authentication/domain/usecases/sign_up_use_case.dart
+++ lib/features/authentication/domain/usecases/sign_up_use_case.dart
@@ -3,7 +3,7 @@ import 'package:injectable/injectable.dart';
 
 import '../../../../core/failures/failures.dart';
 import '../../../../core/usecases/param_use_case.dart';
-import '../../data/models/signup_model%20/signup_request_body.dart';
+import '../../data/models/signup_model/signup_request_body.dart';
 import '../entities/user_entity.dart';
 import '../repositories/authentication_domain_repo.dart';
 
