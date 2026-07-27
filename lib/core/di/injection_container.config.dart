c50d13281a7680241a37555021866fb4c7a854aa
Ibrahim Alnaghi <alnaghy08@gmail.com>
2026-07-27 20:23:52 +0300

    chore(di): regenerate DI container now that build_runner works
    Upgrades the transitive/dev dependencies build_runner (2.8.0 -> 2.15.0),
    args (2.4.2 -> 2.7.0), and build_config (1.2.0 -> 1.3.2), all within the
    existing pubspec.yaml constraints, which fixes the local build_runner /
    Dart SDK incompatibility (missing frontend_server.dart.snapshot).
    
    With build_runner working again, injection_container.config.dart is
    regenerated via `dart run build_runner build --delete-conflicting-outputs`,
    replacing the earlier hand-edited DI registrations with clean codegen
    output. The regenerated file was diffed against the hand-edited version
    and contains identical DI wiring; only cosmetic differences (import-alias
    numbering, registration order, dart format pragma).
    
    flutter analyze and flutter test test/features/search/ (22/22) pass.

--- lib/core/di/injection_container.config.dart
+++ lib/core/di/injection_container.config.dart
@@ -1,4 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
 
 // **************************************************************************
 // InjectableConfigGenerator
@@ -8,149 +9,152 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i4;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+import 'package:dio/dio.dart' as _i361;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
 
 import '../../features/authentication/data/datasources/authentication_data_sources.dart'
-    as _i8;
+    as _i749;
 import '../../features/authentication/data/datasources/authentication_remote_data_source.dart'
-    as _i9;
+    as _i308;
 import '../../features/authentication/data/repositories/authentication_data_repo.dart'
-    as _i11;
-import '../../features/authentication/domain/entities/user_entity.dart' as _i5;
+    as _i360;
+import '../../features/authentication/domain/entities/user_entity.dart'
+    as _i399;
 import '../../features/authentication/domain/repositories/authentication_domain_repo.dart'
-    as _i10;
+    as _i851;
 import '../../features/authentication/domain/usecases/login_use_case.dart'
-    as _i17;
+    as _i938;
 import '../../features/authentication/domain/usecases/rest_password_use_case.dart'
-    as _i22;
+    as _i793;
 import '../../features/authentication/domain/usecases/send_code_use_case.dart'
-    as _i23;
+    as _i804;
 import '../../features/authentication/domain/usecases/sign_up_use_case.dart'
-    as _i28;
+    as _i459;
 import '../../features/authentication/domain/usecases/verify_code_use_case.dart'
-    as _i29;
-import '../../features/cart/data/datasources/cart_data_source.dart' as _i12;
+    as _i121;
+import '../../features/cart/data/datasources/cart_data_source.dart' as _i670;
 import '../../features/cart/data/datasources/remote/cart_remote_data.dart'
-    as _i13;
-import '../../features/cart/data/repositories/cart_data_repo.dart' as _i15;
-import '../../features/cart/domain/repositories/cart_domain_repo.dart' as _i14;
+    as _i882;
+import '../../features/cart/data/repositories/cart_data_repo.dart' as _i451;
+import '../../features/cart/domain/repositories/cart_domain_repo.dart' as _i636;
 import '../../features/cart/domain/usecases/add_product_to_cart_use_case.dart'
-    as _i31;
-import '../../features/cart/domain/usecases/get_cart_use_case.dart' as _i16;
-import '../../features/cart/presentation/bloc/cart_bloc.dart' as _i3;
+    as _i802;
+import '../../features/cart/domain/usecases/get_cart_use_case.dart' as _i488;
+import '../../features/cart/presentation/bloc/cart_bloc.dart' as _i517;
 import '../../features/personalization/data/datasources/personalization_data_source.dart'
-    as _i18;
+    as _i383;
 import '../../features/personalization/data/datasources/remote/personalization_remote_data.dart'
     as _i19;
 import '../../features/personalization/data/repositories/personalization_data_repo.dart'
-    as _i21;
+    as _i391;
 import '../../features/personalization/domain/repositories/personalization_domain_repo.dart'
-    as _i20;
+    as _i101;
 import '../../features/personalization/domain/usecases/add_adress_use_case.dart'
-    as _i30;
+    as _i932;
 import '../../features/personalization/domain/usecases/get_adresses_use_case.dart'
-    as _i33;
+    as _i1013;
 import '../../features/search/data/datasources/remote/search_remote_data.dart'
-    as _i41;
+    as _i697;
 import '../../features/search/data/datasources/search_data_source.dart'
-    as _i40;
-import '../../features/search/data/repositories/search_data_repo.dart'
-    as _i43;
+    as _i545;
+import '../../features/search/data/repositories/search_data_repo.dart' as _i610;
 import '../../features/search/domain/repositories/search_domain_repo.dart'
-    as _i42;
+    as _i853;
 import '../../features/search/domain/usecases/search_products_use_case.dart'
-    as _i44;
+    as _i698;
 import '../../features/shop/data/datasources/remote/shop_remote_data.dart'
-    as _i25;
-import '../../features/shop/data/datasources/shop_data_source.dart' as _i24;
-import '../../features/shop/data/repositories/shop_data_repo.dart' as _i27;
-import '../../features/shop/domain/repositories/shop_domain_repo.dart' as _i26;
-import '../../features/shop/domain/usecases/add_to_fav_use_case.dart' as _i32;
-import '../../features/shop/domain/usecases/get_brands_use_case.dart' as _i34;
+    as _i278;
+import '../../features/shop/data/datasources/shop_data_source.dart' as _i481;
+import '../../features/shop/data/repositories/shop_data_repo.dart' as _i447;
+import '../../features/shop/domain/repositories/shop_domain_repo.dart'
+    as _i1057;
+import '../../features/shop/domain/usecases/add_to_fav_use_case.dart' as _i947;
+import '../../features/shop/domain/usecases/get_brands_use_case.dart' as _i43;
 import '../../features/shop/domain/usecases/get_categories_use_case.dart'
-    as _i35;
-import '../../features/shop/domain/usecases/get_fav_use_case.dart' as _i36;
-import '../../features/shop/domain/usecases/get_products_use_case.dart' as _i37;
-import '../../features/shop/domain/usecases/remove_from_fav_use_case.dart'
-    as _i38;
-import '../../features/shop/presentation/blocs/wishlist/wishlist_bloc.dart'
     as _i6;
-import '../api/api_service.dart' as _i7;
-import 'injection_container.dart' as _i39;
+import '../../features/shop/domain/usecases/get_fav_use_case.dart' as _i691;
+import '../../features/shop/domain/usecases/get_products_use_case.dart'
+    as _i450;
+import '../../features/shop/domain/usecases/remove_from_fav_use_case.dart'
+    as _i1066;
+import '../../features/shop/presentation/blocs/wishlist/wishlist_bloc.dart'
+    as _i849;
+import '../api/api_service.dart' as _i299;
+import 'injection_container.dart' as _i809;
 
-extension GetItInjectableX on _i1.GetIt {
+extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
+  _i174.GetIt init({
     String? environment,
-    _i2.EnvironmentFilter? environmentFilter,
+    _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i2.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
     final registerModule = _$RegisterModule();
-    gh.lazySingleton<_i3.CartBloc>(() => registerModule.cartBloc);
-    gh.lazySingleton<_i4.Dio>(() => registerModule.dio);
-    gh.factory<_i5.UserEntity>(() => registerModule.userData);
-    gh.lazySingleton<_i6.WishlistBloc>(() => registerModule.wishlistBloc);
-    gh.lazySingleton<_i7.ApiService>(() => _i7.ApiService(gh<_i4.Dio>()));
-    gh.lazySingleton<_i8.AuthenticationDataSources>(() =>
-        _i9.AuthenticationRemoteDataSources(apiService: gh<_i7.ApiService>()));
-    gh.lazySingleton<_i10.AuthenticationDomainRepo>(
-        () => _i11.AuthenticationDataRepo(gh<_i8.AuthenticationDataSources>()));
-    gh.lazySingleton<_i12.CartDataSource>(
-        () => _i13.PersonalizationRemoteData(gh<_i7.ApiService>()));
-    gh.lazySingleton<_i14.CartDomainRepo>(
-        () => _i15.CartDataRepo(gh<_i12.CartDataSource>()));
-    gh.lazySingleton<_i16.GetCartUseCase>(
-        () => _i16.GetCartUseCase(gh<_i14.CartDomainRepo>()));
-    gh.lazySingleton<_i17.LoginUseCase>(
-        () => _i17.LoginUseCase(gh<_i10.AuthenticationDomainRepo>()));
-    gh.lazySingleton<_i18.PersonalizationDataSource>(
-        () => _i19.PersonalizationRemoteData(gh<_i7.ApiService>()));
-    gh.lazySingleton<_i20.PersonalizationDomainRepo>(() =>
-        _i21.PersonalizationDataRepo(gh<_i18.PersonalizationDataSource>()));
-    gh.lazySingleton<_i22.RestPasswordUseCase>(
-        () => _i22.RestPasswordUseCase(gh<_i10.AuthenticationDomainRepo>()));
-    gh.lazySingleton<_i23.SendCodeUseCase>(
-        () => _i23.SendCodeUseCase(gh<_i10.AuthenticationDomainRepo>()));
-    gh.lazySingleton<_i24.ShopDataSource>(
-        () => _i25.ShopRemoteData(gh<_i7.ApiService>()));
-    gh.lazySingleton<_i26.ShopDomainRepo>(
-        () => _i27.ShopDataRepo(gh<_i24.ShopDataSource>()));
-    gh.lazySingleton<_i28.SignupUseCase>(
-        () => _i28.SignupUseCase(gh<_i10.AuthenticationDomainRepo>()));
-    gh.lazySingleton<_i29.VerifyCodeUseCase>(
-        () => _i29.VerifyCodeUseCase(gh<_i10.AuthenticationDomainRepo>()));
-    gh.lazySingleton<_i30.AddAdressUseCase>(
-        () => _i30.AddAdressUseCase(gh<_i20.PersonalizationDomainRepo>()));
-    gh.lazySingleton<_i31.AddProductToCart>(
-        () => _i31.AddProductToCart(gh<_i14.CartDomainRepo>()));
-    gh.lazySingleton<_i32.AddToFavUseCase>(
-        () => _i32.AddToFavUseCase(gh<_i26.ShopDomainRepo>()));
-    gh.lazySingleton<_i33.GetAdressesUseCase>(
-        () => _i33.GetAdressesUseCase(gh<_i20.PersonalizationDomainRepo>()));
-    gh.lazySingleton<_i34.GetBrandsUseCase>(
-        () => _i34.GetBrandsUseCase(gh<_i26.ShopDomainRepo>()));
-    gh.lazySingleton<_i35.GetCategoriesUseCase>(
-        () => _i35.GetCategoriesUseCase(gh<_i26.ShopDomainRepo>()));
-    gh.lazySingleton<_i36.GetFavUseCase>(
-        () => _i36.GetFavUseCase(gh<_i26.ShopDomainRepo>()));
-    gh.lazySingleton<_i37.GetProductsUseCase>(
-        () => _i37.GetProductsUseCase(gh<_i26.ShopDomainRepo>()));
-    gh.lazySingleton<_i38.RemoveFromFavUseCase>(
-        () => _i38.RemoveFromFavUseCase(gh<_i26.ShopDomainRepo>()));
-    gh.lazySingleton<_i40.SearchDataSource>(
-        () => _i41.SearchRemoteData(gh<_i7.ApiService>()));
-    gh.lazySingleton<_i42.SearchDomainRepo>(
-        () => _i43.SearchDataRepo(gh<_i40.SearchDataSource>()));
-    gh.lazySingleton<_i44.SearchProductsUseCase>(
-        () => _i44.SearchProductsUseCase(gh<_i42.SearchDomainRepo>()));
+    gh.factory<_i399.UserEntity>(() => registerModule.userData);
+    gh.lazySingleton<_i361.Dio>(() => registerModule.dio);
+    gh.lazySingleton<_i849.WishlistBloc>(() => registerModule.wishlistBloc);
+    gh.lazySingleton<_i517.CartBloc>(() => registerModule.cartBloc);
+    gh.lazySingleton<_i299.ApiService>(() => _i299.ApiService(gh<_i361.Dio>()));
+    gh.lazySingleton<_i545.SearchDataSource>(
+        () => _i697.SearchRemoteData(gh<_i299.ApiService>()));
+    gh.lazySingleton<_i383.PersonalizationDataSource>(
+        () => _i19.PersonalizationRemoteData(gh<_i299.ApiService>()));
+    gh.lazySingleton<_i481.ShopDataSource>(
+        () => _i278.ShopRemoteData(gh<_i299.ApiService>()));
+    gh.lazySingleton<_i1057.ShopDomainRepo>(
+        () => _i447.ShopDataRepo(gh<_i481.ShopDataSource>()));
+    gh.lazySingleton<_i670.CartDataSource>(
+        () => _i882.PersonalizationRemoteData(gh<_i299.ApiService>()));
+    gh.lazySingleton<_i749.AuthenticationDataSources>(() =>
+        _i308.AuthenticationRemoteDataSources(
+            apiService: gh<_i299.ApiService>()));
+    gh.lazySingleton<_i853.SearchDomainRepo>(
+        () => _i610.SearchDataRepo(gh<_i545.SearchDataSource>()));
+    gh.lazySingleton<_i101.PersonalizationDomainRepo>(() =>
+        _i391.PersonalizationDataRepo(gh<_i383.PersonalizationDataSource>()));
+    gh.lazySingleton<_i851.AuthenticationDomainRepo>(() =>
+        _i360.AuthenticationDataRepo(gh<_i749.AuthenticationDataSources>()));
+    gh.lazySingleton<_i932.AddAdressUseCase>(
+        () => _i932.AddAdressUseCase(gh<_i101.PersonalizationDomainRepo>()));
+    gh.lazySingleton<_i1013.GetAdressesUseCase>(
+        () => _i1013.GetAdressesUseCase(gh<_i101.PersonalizationDomainRepo>()));
+    gh.lazySingleton<_i698.SearchProductsUseCase>(
+        () => _i698.SearchProductsUseCase(gh<_i853.SearchDomainRepo>()));
+    gh.lazySingleton<_i947.AddToFavUseCase>(
+        () => _i947.AddToFavUseCase(gh<_i1057.ShopDomainRepo>()));
+    gh.lazySingleton<_i43.GetBrandsUseCase>(
+        () => _i43.GetBrandsUseCase(gh<_i1057.ShopDomainRepo>()));
+    gh.lazySingleton<_i6.GetCategoriesUseCase>(
+        () => _i6.GetCategoriesUseCase(gh<_i1057.ShopDomainRepo>()));
+    gh.lazySingleton<_i691.GetFavUseCase>(
+        () => _i691.GetFavUseCase(gh<_i1057.ShopDomainRepo>()));
+    gh.lazySingleton<_i450.GetProductsUseCase>(
+        () => _i450.GetProductsUseCase(gh<_i1057.ShopDomainRepo>()));
+    gh.lazySingleton<_i1066.RemoveFromFavUseCase>(
+        () => _i1066.RemoveFromFavUseCase(gh<_i1057.ShopDomainRepo>()));
+    gh.lazySingleton<_i938.LoginUseCase>(
+        () => _i938.LoginUseCase(gh<_i851.AuthenticationDomainRepo>()));
+    gh.lazySingleton<_i793.RestPasswordUseCase>(
+        () => _i793.RestPasswordUseCase(gh<_i851.AuthenticationDomainRepo>()));
+    gh.lazySingleton<_i804.SendCodeUseCase>(
+        () => _i804.SendCodeUseCase(gh<_i851.AuthenticationDomainRepo>()));
+    gh.lazySingleton<_i459.SignupUseCase>(
+        () => _i459.SignupUseCase(gh<_i851.AuthenticationDomainRepo>()));
+    gh.lazySingleton<_i121.VerifyCodeUseCase>(
+        () => _i121.VerifyCodeUseCase(gh<_i851.AuthenticationDomainRepo>()));
+    gh.lazySingleton<_i636.CartDomainRepo>(
+        () => _i451.CartDataRepo(gh<_i670.CartDataSource>()));
+    gh.lazySingleton<_i802.AddProductToCart>(
+        () => _i802.AddProductToCart(gh<_i636.CartDomainRepo>()));
+    gh.lazySingleton<_i488.GetCartUseCase>(
+        () => _i488.GetCartUseCase(gh<_i636.CartDomainRepo>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i39.RegisterModule {}
+class _$RegisterModule extends _i809.RegisterModule {}
